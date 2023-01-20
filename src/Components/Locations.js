5e1c64c05db7fb98e90ed8ab8b80e2572b84873f
Marlon Leon <ghostveene@gmail.com>
2023-01-20 06:10:25 -0500

    added extra div to seperate location

--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -5,9 +5,11 @@ export default function Locations(props) {
         <div className="location-section">
             <img src={props.imageUrl} className="location-img"></img>
             <div className="location-info-section">
-                <IoLocationSharp className="location-icon"/>
-                <h3 className="location-country">{props.location}</h3>
-                <a href={props.googleMapsUrl} className="google-maps">View on Google Maps</a>
+                <div className="location-top-section">
+                    <IoLocationSharp className="location-icon"/>
+                    <h3 className="location-country">{props.location}</h3>
+                    <a href={props.googleMapsUrl} className="google-maps">View on Google Maps</a>
+                </div>
                 <h1 className="location-title">{props.title}</h1>
                 <h4 className="location-dates">{props.startDate} - {props.endDate}</h4>
                 <p className="location-description">{props.description}</p>
