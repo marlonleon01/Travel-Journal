5be4d0096fdbeb42a4e00c6cc551c6e4ffef2bab
Marlon Leon <ghostveene@gmail.com>
2023-01-20 04:42:47 -0500

    added icon and title

---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import {IoEarth} from "react-icons/io5"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">  
+            <IoEarth className="navbar-icon"/>
+            <h1 className="navbar-title">my travel journal</h1>
+        </nav> 
+    )
+}
