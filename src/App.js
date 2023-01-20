638fb4522cab6c1480c39130b84d484e9902eef9
Marlon Leon <ghostveene@gmail.com>
2023-01-20 05:10:45 -0500

    mapping through data.js to add to locations.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Navbar from "./Components/Navbar"
+import Locations from "./Components/Locations";
+import data from "./Components/data"
 
 export default function App() {
+  const locationInfo = data.map(items => {
+    return <Locations 
+            key={items.id}
+            {...items} 
+            />
+  })
+  
   return (
     <div className="all-content">
       <Navbar />
+      {locationInfo}
     </div>
   );
 }
