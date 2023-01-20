377723561830795429031685cfc1a08e67098750
Marlon Leon <ghostveene@gmail.com>
2023-01-20 04:53:59 -0500

    changed to h3 to match template

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav className="navbar">  
             <IoEarth className="navbar-icon"/>
-            <h1 className="navbar-title">my travel journal</h1>
+            <h3 className="navbar-title">my travel journal</h3>
         </nav> 
     )
 }
